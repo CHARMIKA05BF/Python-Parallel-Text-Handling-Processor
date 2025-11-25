969f6f7332c031772413e0c0167f3acc1c98f2e3
Bella Gnana Charmika <gnanachamikabella@gmail.com>
2025-11-25 16:53:38 +0530

    Add files via upload

---
+++ app/checker/__init__.py
@@ -0,0 +1,11 @@
+from .checker import Checker, score_text, count_words
+from .rules import load_rules, evaluate_rule
+
+__all__ = [
+	"Checker",
+	"score_text",
+	"count_words",
+	"load_rules",
+	"evaluate_rule",
+]
+
